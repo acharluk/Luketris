d8b677f9475e2ec587617ac06b172902930504fb
ACharLuk <acharluk@gmail.com>
2015-03-02 15:15:07 +0100

    Created Idea project

---
+++ src/com/acharluk/luketris/Luketris.java
@@ -0,0 +1,18 @@
+package com.acharluk.luketris;
+
+import javax.swing.*;
+
+/**
+ * Created by ACharLuk on 02/03/2015.
+ */
+public class Luketris extends JFrame {
+
+    public Luketris() {
+
+    }
+
+    public static void main (String[] args) {
+        new Luketris();
+    }
+
+}
